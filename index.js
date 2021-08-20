6a9f09cf42a7fe6bb7839666c4b29fd4a2f6618d
w <none>
2021-08-20 02:48:40 +0000

    working copy snapshot

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+const express = require('express');
+const fetch = require('node-fetch');
+
+const feedP = (async () => {
+  try {
+  console.log('checking for cached feed');
+    const feed = await fs.promises.readFile('/tmp/feed.xml', 'utf-8');
+    console.log('found, read');
+    return feed;
+  } catch (e) {
+    if (e.code !== 'ENOENT') throw e;
+    console.log('not found');
+  }
+  console.log('downloading');
+  const res = await fetch('https://www.reddit.com/domain/v.redd.it/.rss');
+  const feed = await res.text();
+  console.log('done, caching');
+  await fs.promises.writeFile('/tmp/feed.xml.tmp', feed, 'utf-8');
+  await fs.promises.rename('/tmp/feed.xml.tmp', '/tmp/feed.xml');
+  console.log('done');
+  return feed;
+})();
+
+const app = express();
+app.use(express.static('public'));
+app.get('/videos.xml', async (req, res, next) => {
+  try {
+    const feed = await feedP;
+    res.end(feed);
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.listen(process.env.PORT);
